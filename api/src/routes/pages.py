29ea63ad2fa3197ab046443b56ade8938357f670
ErwanDuprey <erwan.duprey@free.fr>
2024-04-03 23:29:12 +0200

    Login and register added. Need to be fixed

--- api/src/routes/pages.py
+++ api/src/routes/pages.py
@@ -1,54 +1,79 @@
+import datetime
 from typing import Annotated
 
-from fastapi import FastAPI, Request, UploadFile, File, HTTPException, APIRouter, status, Depends, Form
-from fastapi.responses import HTMLResponse, FileResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi import Request, HTTPException, APIRouter, status, Depends, Form, Cookie, Response
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from passlib.context import CryptContext
 from ..config.db_config import ConfigDB
+from datetime import timezone, timedelta
 import os
 import psycopg2
+import string
+import random
 
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# TODO Add salt mandatory
+pwd_context = CryptContext(schemes=["md5_crypt"], deprecated="auto")
 
 router = APIRouter(prefix="/pages",
     tags=["pages"],
     responses={404: {"description": "Not found"}},)
 templates = Jinja2Templates(directory="../templates/")
-security = HTTPBasic()
 
 
 class User:
-    def __init__(self, email, name, forename, password, role):
+    def __init__(self, email, password, name, forename, role, cookie):
         self.email = email
+        self.password = password
         self.name = name
         self.forename = forename
-        self.password = password
         self.role = role
+        self.cookie = cookie
 
     def verify_password(self, plain_password):
+        print(plain_password)
+        print(self.password)
+        print(pwd_context.verify(plain_password, self.password))
         return pwd_context.verify(plain_password, self.password)
 
 
-def get_user(email, cursor):
-    cursor.execute("SELECT email, name, forename, password, role FROM USERS WHERE email = %s", (email,))
+def get_user(cursor, email=None, cookie=None):
+    SQL_query = f"SELECT email, password, name, forename, role, cookie FROM USERS WHERE 1 = 1"
+    if email is not None:
+        SQL_query += f" AND email = '{email}' "
+    if cookie is not None:
+        SQL_query += f" AND cookie = '{cookie}'"
+    cursor.execute(SQL_query)
     user_data = cursor.fetchone()
+    print(SQL_query)
     if user_data:
-        return User(user_data['email'], user_data['name'], user_data['forename'], user_data['password'], user_data['role'])
+        return User(user_data['email'], user_data['password'], user_data['name'], user_data['forename'],  user_data['role'], user_data['cookie'])
     return None
 
 
+def insert_user(connector, cursor, email, password, name, forename):
+    cursor.execute(f"INSERT INTO users(email, password, name, forename, role, cookie) VALUES('{email}', '{password}', '{name}', '{forename}', 2, '')")
+    connector.commit()
+    letters = string.ascii_lowercase
+    cookie_value = ''.join(random.choice(letters) for _ in range(255))
+    set_cookie(connector, cursor, email, cookie_value)
+    return User(email, password, name, forename, 2, cookie_value)
+
+
 def get_current_user_role(email: str, cursor=Depends(ConfigDB().get_db_cursor())):
-    user = get_user(email, cursor)
+    user = get_user(cursor, email)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid username or password",
-            headers={"WWW-Authenticate": "Basic"},
+            detail="Invalid username or password"
         )
     return user.role
+
+
+def set_cookie(connector, cursor, email, cookie_value):
+    sql = f"UPDATE USERS SET cookie = '{cookie_value}' WHERE email = '{email}'"
+    cursor.execute(sql)
+    connector.commit()
 
 
 @router.get("/about", response_class=HTMLResponse)
@@ -58,48 +83,90 @@ async def about(request: Request):
     )
 
 
+@router.get("/dashboard", response_class=HTMLResponse)
+async def dashboard(request: Request):
+    cookie_value = request.cookies.get('ICARUS-Login')
+    print(cookie_value)
+    if cookie_value is not None:
+        DB = ConfigDB()
+        cursor = DB.get_db_cursor()
+        if get_user(cursor, cookie=cookie_value) is not None:
+            cursor.close()
+            DB.connector.close()
+            return templates.TemplateResponse(
+                name="dashboard.html", context={"request": request}
+            )
+        cursor.close()
+        DB.connector.close()
+    return templates.TemplateResponse(
+        name="login.html", context={"request": request}
+    )
+
+
 @router.get("/login", response_class=HTMLResponse)
 async def about(request: Request):
     return templates.TemplateResponse(
         name="login.html", context={"request": request}
     )
 
-"""
-@router.post("/login")
-async def login(credentials: HTTPBasicCredentials = Depends(security)):
+
+@router.get("/logout", response_class=HTMLResponse)
+def logout(response: Response, request: Request):
+    response.delete_cookie(key="ICARUS-Login")
+    return templates.TemplateResponse(
+        name="index.html", context={"request": request}
+    )
+
+
+@router.get("/register", response_class=HTMLResponse)
+async def create_user(request: Request):
+    return templates.TemplateResponse(
+        name="register.html", context={"request": request}
+    )
+
+
+@router.post("/register", response_class=HTMLResponse)
+async def register(email: Annotated[str, Form()], password: Annotated[str, Form()], name: Annotated[str, Form()], forename: Annotated[str, Form()], response: Response, request: Request):
     DB = ConfigDB()
     cursor = DB.get_db_cursor()
-    user = get_user(credentials.username, cursor)
-
-    if not user or not user.verify_password(credentials.password):
+    if get_user(cursor, email) is not None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid username or password",
-            headers={"WWW-Authenticate": "Basic"},
+            detail="User already exists"
         )
-    cursor.close()
-    DB.connector.close()
-
-    return {"email": user.email, "role": user.role}
-"""
-
-@router.post("/login")
-async def login(email: Annotated[str, Form()], password: Annotated[str, Form()]):
-    DB = ConfigDB()
-    cursor = DB.get_db_cursor()
-    user = get_user(email, cursor)
-
+    user = insert_user(DB.connector, cursor, email, pwd_context.hash(password, scheme="md5_crypt"), name, forename)
     if not user or not user.verify_password(password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid username or password",
-            headers={"WWW-Authenticate": "Basic"},
+            detail="Invalid username or password"
         )
     cursor.close()
     DB.connector.close()
-    raise HTTPException(
-        status_code=status.HTTP_200_OK,
-        detail={"email": user.email, "role": user.role},
-        headers={"WWW-Authenticate": "Basic"},
+    expiry = datetime.datetime.now(datetime.timezone.utc) + timedelta(days=1)
+    response.set_cookie(key="ICARUS-Login", value=f"{user.cookie}", expires=expiry, secure=True, samesite="none")
+    return templates.TemplateResponse(
+        name="index.html", context={"request": request}
     )
 
+
+@router.post("/login")
+async def login(email: Annotated[str, Form()], password: Annotated[str, Form()], response: Response, request: Request):
+    DB = ConfigDB()
+    cursor = DB.get_db_cursor()
+    user = get_user(cursor, email)
+    if not user or not user.verify_password(password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid username or password"
+        )
+
+    letters = string.ascii_lowercase
+    cookie_value = ''.join(random.choice(letters) for _ in range(255))
+    set_cookie(DB.connector, cursor, email, cookie_value)
+    cursor.close()
+    DB.connector.close()
+    expiry = datetime.datetime.now(datetime.timezone.utc) + timedelta(days=1)
+    response.set_cookie(key="ICARUS-Login", value=f"{cookie_value}", expires=expiry, secure=True, samesite="none")
+    return templates.TemplateResponse(
+        name="dashboard.html", context={"request": request}
+    )
