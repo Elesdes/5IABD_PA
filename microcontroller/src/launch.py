50a9b8f29914b04e441ea3031785f7402708eb20
Leonardeaux <enzoleonardo@hotmail.fr>
2024-06-23 22:37:46 +0200

    Adding download of the model from cloud

--- microcontroller/src/launch.py
+++ microcontroller/src/launch.py
@@ -43,49 +43,49 @@ print("Model loaded")
 with open("/home/enzol/Documents/5IABD_PA/microcontroller/src/state.txt", "w") as f:
     f.write("Model loaded")
 
-# m = Myo(sys.argv[1] if len(sys.argv) >= 2 else None, mode=emg_mode.RAW)
+m = Myo(sys.argv[1] if len(sys.argv) >= 2 else None, mode=emg_mode.RAW)
 
-# myo_data = []
+myo_data = []
 
-# def proc_emg(emg, moving, times=[]):
-#     myo_data.append(emg)
+def proc_emg(emg, moving, times=[]):
+    myo_data.append(emg)
 
-# m.add_emg_handler(proc_emg)
-# m.connect()
+m.add_emg_handler(proc_emg)
+m.connect()
 
-# m.add_arm_handler(lambda arm, xdir: print("arm", arm, "xdir", xdir))
-# m.add_pose_handler(lambda p: print("pose", p))
+m.add_arm_handler(lambda arm, xdir: print("arm", arm, "xdir", xdir))
+m.add_pose_handler(lambda p: print("pose", p))
 
-# m.sleep_mode(1)
-# m.set_leds([128, 128, 255], [128, 128, 255])  # purple logo and bar LEDs
-# m.vibrate(1)
-# print(f"{'*'* 10}Connected{'*'* 10}")
+m.sleep_mode(1)
+m.set_leds([128, 128, 255], [128, 128, 255])  # purple logo and bar LEDs
+m.vibrate(1)
+print(f"{'*'* 10}Connected{'*'* 10}")
 
-# myo_cols = [
-#     "sensor1",
-#     "sensor2",
-#     "sensor3",
-#     "sensor4",
-#     "sensor5",
-#     "sensor6",
-#     "sensor7",
-#     "sensor8",
-# ]
-# chunk_size = 300
+myo_cols = [
+    "sensor1",
+    "sensor2",
+    "sensor3",
+    "sensor4",
+    "sensor5",
+    "sensor6",
+    "sensor7",
+    "sensor8",
+]
+chunk_size = 300
 
-# try:
-#     while True:
-#         m.run()
+try:
+    while True:
+        m.run()
 
-#         if len(myo_data) == chunk_size:
-#             data = pd.DataFrame(myo_data, columns=myo_cols)
-#             prediction = predict(model, data)
+        if len(myo_data) == chunk_size:
+            data = pd.DataFrame(myo_data, columns=myo_cols)
+            prediction = predict(model, data)
 
-#             print(prediction)
-#             myo_data = []
+            print(prediction)
+            myo_data = []
 
-#             hand.moveFromCategoricalList(prediction)
+            hand.moveFromCategoricalList(prediction)
 
-# except KeyboardInterrupt:
-#     m.disconnect()
-#     quit()
+except KeyboardInterrupt:
+    m.disconnect()
+    quit()
