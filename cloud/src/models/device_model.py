c4d6993226bd631d71cb0fc2cada22cf4fd52500
ErwanDuprey <erwan.duprey@free.fr>
2024-07-03 17:47:36 +0200

    Patch DeviceModel on devices_services.py 5

--- cloud/src/models/device_model.py
+++ cloud/src/models/device_model.py
@@ -8,6 +8,7 @@ class DeviceModel(BaseModel):
     @field_validator("device_id")
     def validate_id_format(cls, device_id: str):
         try:
+            print(device_id)
             pattern = re.compile(r'^[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}$')
             if not pattern.match(device_id):
                 raise ValueError("ID must be exactly 19 digits with 3 -")
