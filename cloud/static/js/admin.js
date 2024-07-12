4f5948749d5cb43060320dd3d25a4d4fffde7550
ErwanDuprey <erwan.duprey@free.fr>
2024-07-12 17:28:49 +0200

    Reverse fix admin.js

--- cloud/static/js/admin.js
+++ cloud/static/js/admin.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch("/api/get_role")
         .then(response => response.json())
         .then(data => {
-            if (data == true) {
+            if (data) {
                 document.querySelectorAll('.admin-only').forEach(element => {
                     element.style.visibility = 'visible';
                 });
